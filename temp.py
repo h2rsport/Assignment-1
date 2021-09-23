7385edeee04007c4b7d1f08e9a3280bef2051414
h2rsport <91111903+h2rsport@users.noreply.github.com>
2021-09-24 00:42:14 +0530

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,8 @@
+print("What u want to calc. fahrenheit to celcius or celcius to farenheit")
+Celcius = float(input("Enter temperature in celcius: "))
+Fahrenheit =(Celcius *9/5) +32
+print(Fahrenheit)
+
+Fahrenheit = float(input("Enter temperature in fahrenheit:  "))
+Celcius = (Fahrenheit -32) * 5/9
+print(Celcius)
